a8319bd7f1d3144d5b193f784f66f0dab3736285
broline <brolineoronu3@gmail.com>
2023-05-09 16:02:49 -0700

    top

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-"""a recursive function that queries the Reddit API and returns a list containing
-the titles of all hot articles for a given subreddit. If no results are found for
-the given subreddit, the function should return None.
-"""
-
+"""2-recurse module"""
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
-    """returns a list containing the titles of all hot articles 
-    for a given subreddit."""
-    reddit_url = "https://www.reddit.com/r/" + subreddit
-    reddit_url2 = reddit_url + "/hot.json?limit=100&after=after"
-    header = {"User-Agent": "Chrome/81.0.4044.129"}
-    req = requests.get(reddit_url2, headers=header)
-    reddit = req.json()
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """
+    Queries the Reddit API
 
-    if (req.status_code == 200):
-        """checks if the response status is ok"""
-        hot_post = reddit.get("data").get("children")
-        after = reddit.get("data").get("after")
-        for posts in hot_post:
-            hot_list.append(posts.get("data").get("title"))
-        if after is None:
-            recurse(subreddit, hot_list, after)
-        return hot_list
-    else:
+    Returns:
+        list containing the titles of all hot articles for a given subreddit,
+        None if an invalid subreddit is given
+    """
+    URL = 'https://www.reddit.com/r/'
+    res = requests.get('{}{}/hot.json?limit=10'.
+                       format(URL, subreddit),
+                       params={"count": count, "after": after},
+                       headers={'User-Agent': 'ALX-User-Agent'},
+                       allow_redirects=False)
+
+    if res.status_code >= 400:
         return None
+
+    list = hot_list + [child.get("data").get("title")
+                       for child in res.json()
+                       .get("data")
+                       .get("children")]
+
+    data = res.json()
+    if not data.get("data").get("after"):
+        return list
+
+    return recurse(subreddit, list, data.get("data").get("count"),
+                   data.get("data").get("after"))
