93eae99b8942c876f7395086acc1592c738f1b1a
broline <brolineoronu3@gmail.com>
2023-05-18 16:38:57 -0700

    limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,18 +1,7 @@
-# Change the OS configuration so that it is possible to login with 
-# the holberton user and open a file without any error message.
-file { '/etc/security/limits.conf':
-  ensure  => present,
-  content => "holberton hard nofile 88888\nholberton soft nofile 88888\n",
-}
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message.
 
-exec { 'reload-limits-configuration':
-  command => 'pam-auth-update --force',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  refreshonly => true,
-}
-
-service { 'systemd-logind':
-  ensure => running,
-  enable => true,
-  require => [File['/etc/security/limits.conf'], Exec['reload-limits-configuration']],
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
